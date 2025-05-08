c483ac475d2e9f3a24a0cd68e5bcb956d65627b0
Franklin William <ui.develloper@gmail.com>
2025-05-08 13:50:54 -0300

    Implementação do componente de slide 'Swiper'

--- components/common/ClientLogo.tsx
+++ components/common/ClientLogo.tsx
@@ -1,24 +1,39 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import { clientLogo } from "@/data/client";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 
 const ClientLogo = () => {
   return (
     <>
-      <div className="overflow-x-hidden">
-        <ul className="flex-center gap-5 animate-infinite-scroll p-10">
-          {clientLogo.map((client, index) => (
-            <li key={index} className="min-w-[260px] h-[140px] bg-background rounded-2xl p-4 flex-center card-shadow">
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={50}
+        slidesPerView={4}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        className="py-10"
+      >
+        {clientLogo.map((client, index) => (
+          <SwiperSlide key={index} className="py-10">
+            <div className="min-w-[260px] h-[140px] bg-background rounded-2xl p-4 flex-center card-shadow">
               <Image
                 src={client.src}
                 width={160}
                 height={140}
                 alt={client.alt}
               />
-            </li>
-          ))}
-        </ul>
-      </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 };
